5f048d62990dbf73069146e1b4792a8648703178
lorenzo <lorenzocircelli5@gmail.com>
2025-04-10 00:34:57 -0300

    Main function to integrate list recipes and resources to uppercase

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -3,13 +3,14 @@ package org.main;
 import org.materials.*;
 import org.recipes.*;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         boolean status = true;
-        int option = 0;
+        int option;
 
         Scanner input = new Scanner(System.in);
 
@@ -31,7 +32,7 @@ public class Main {
 
             String answer;
 
-            System.out.println("1 - Pesquisar por Receita");
+            System.out.println("\n\n1 - Pesquisar por Receita");
             System.out.println("2 - Listar as Receitas");
             System.out.println("9 - Sair");
 
@@ -42,10 +43,30 @@ public class Main {
                 case 1:
 
                     System.out.println("Insira a receita que deseja procurar: ");
+                    input.nextLine();
                     answer = input.nextLine();
+
+                    System.out.println();
+
                     break;
 
                 case 2:
+
+                    Map<String, Recipes> allRecipes = RecipeLoader.loadFromCsv("resources/data/recipes.csv");
+
+                    for(Recipes recipe : allRecipes.values()){
+
+                        System.out.println("\nName: " +recipe.getName());
+                        System.out.println("Category: " +recipe.getCategory());
+                        for(var entry : recipe.getResources().entrySet()){
+
+                            System.out.println("Resource: " +entry.getKey() +  " - " + entry.getValue());
+
+                        }
+                        System.out.println("Description " +recipe.getDescription());
+                        System.out.println("Note:  " +recipe.getNote());
+
+                    }
 
                     break;
 
