d46a41735130757db30b7ea80c9db049a318d9c6
lorenzo <lorenzocircelli5@gmail.com>
2025-04-10 18:53:14 -0300

    Improved search, build base created and recipes updated

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -14,7 +14,6 @@ public class Main {
 
         Map<String, Recipes> allRecipes = RecipeLoader.loadFromCsv("resources/data/recipes.csv");
 
-
         Scanner input = new Scanner(System.in);
 
         System.out.println("""
@@ -35,8 +34,9 @@ public class Main {
 
             String answer;
 
-            System.out.println("\n\n1 - Pesquisar por Receita");
+            System.out.println("1 - Pesquisar por Receita");
             System.out.println("2 - Listar as Receitas");
+            System.out.println("3 - Gerador de Bases");
             System.out.println("9 - Sair");
 
             option = input.nextInt();
@@ -47,11 +47,11 @@ public class Main {
 
                     System.out.println("Insira a receita que deseja procurar: ");
                     input.nextLine();
-                    answer = input.nextLine();
+                    answer = input.nextLine().toUpperCase();
 
                     for(Recipes recipe :  allRecipes.values()){
 
-                        if(recipe.getName().equals(answer)){
+                        if(recipe.getName().toUpperCase().equals(answer)){
 
                             System.out.println("Recipe: ");
                             System.out.println(recipe.getName());
@@ -73,7 +73,6 @@ public class Main {
 
                 case 2:
 
-
                     for(Recipes recipe : allRecipes.values()){
 
                         System.out.println("\nName: " +recipe.getName());
@@ -85,6 +84,54 @@ public class Main {
                         }
                         System.out.println("Description " +recipe.getDescription());
                         System.out.println("Note:  " +recipe.getNote());
+
+                    }
+
+                    break;
+
+                case 3:
+
+                    System.out.println("Build a project:");
+
+                    System.out.println("\n\n1 - By Area");
+                    System.out.println("2 - By Advancement");
+                    System.out.println("3 - By size");
+                    System.out.println("4 - Developer Choice");
+
+                    int answ2 = input.nextInt();
+                    switch(answ2){
+
+                        case 1:
+
+                            System.out.println("1 - Kelp Forest");
+                            System.out.println("2 - Grassy Plateaus");
+                            System.out.println("3 - Mushroom Forest (Next to Lost River Entrance");
+                            System.out.println("4 - Bubble Zone (Below Floating Island)");
+                            System.out.println("5 - Lost River");
+                            System.out.println("6 - Inactive Lava Zone");
+                            System.out.println("9 - Sair");
+
+                            int answ3 = input.nextInt();
+
+                            switch(answ3){
+
+                                case 1:
+
+
+
+                            }
+
+                            break;
+
+                        case 2:
+                            System.out.println("1 - Early Game (Basic Areas, next to the surface)");
+                            System.out.println("2 - Mid Game (Medium Areas, between 300m and 900m");
+                            System.out.println("3 - Late Game (Advanced Areas, unlocked Lava Zones");
+                            System.out.println("4 - End Game (Reached every resource needed)");
+
+                            answ3 = input.nextInt();
+
+                            break;
 
                     }
 
